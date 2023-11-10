9dfab7344b9a3c7676b91b18d2fe995444ce4113
lucasvillarrubia <villar.lucase@gmail.com>
2023-11-10 20:16:43 -0300

    Cambio de directorio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/header/Navbar"
+import Toolbar from "./components/header/Toolbar"
+import Footer from "./components/footer/Footer"
+import Cart from "./components/cart/Cart"
+import Menu from "./components/menu/Menu"
+import Routes from "./routes/Routes"
+import Layout from "./components/layout/Layout"
+
+function App() {
+        
+        return (
+                <Layout>
+                        <Navbar />
+                        {/* <Menu /> */}
+                        <Routes />
+                        {/* <Toolbar /> */}
+                        <Footer />
+                </Layout>
+        )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App.jsx'
+import App from './App.jsx'
 import { GlobalStyles } from './styles/GlobalStyles.js'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
